91810c97798c8283f8211c01784951b61998d187
utkarshp <utkarshp@azuga.com>
2026-04-03 17:15:13 +0530

    added webpack for control build

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const ZipPlugin = require("zip-webpack-plugin");
+
+const entries = {
+  chatbotAgent: "./chatbotAgent/index.ts",
+};
+
+module.exports = {
+  entry: entries,
+  target: "node",
+  mode: "production",
+  externals: [
+    nodeExternals({
+      importType: "module",
+      modulesDir: path.resolve("./node_modules"),
+    }),
+    "@aws-sdk",
+  ],
+  experiments: {
+    outputModule: true,
+  },
+  output: {
+    path: path.join(__dirname, "dist/"),
+    filename: "[name]/index.js",
+    library: {
+      type: "module",
+    },
+    chunkFormat: "module",
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".mjs"],
+    alias: {},
+    modules: [path.resolve("./"), "node_modules"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: [
+          [
+            path.resolve(__dirname, ".serverless"),
+            path.resolve(__dirname, "node_modules"),
+            path.resolve(__dirname, ".webpack"),
+            path.resolve(__dirname, "dist"),
+          ],
+        ],
+      },
+    ],
+  },
+  plugins: Object.keys(entries).map(
+    (name) =>
+      new ZipPlugin({
+        filename: `${name}.zip`,
+        include: [`${name}/`],
+        path: path.resolve(__dirname, "dist"),
+      })
+  ),
+  // optimization: {
+  //     minimize: false,
+  //     minimizer: [
+  //         new TerserPlugin({
+  //             terserOptions: {
+  //                 ecma: 2020,
+  //             },
+  //         }),
+  //     ],
+  // },
+};
